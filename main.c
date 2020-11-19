e860c51f89c5a4b58d35e34a26ecc1d2cac1695e
jaswindersroya <74692394+jaswindersroya@users.noreply.github.com>
2020-11-18 22:02:10 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+// 1.calulate the area of rectangle
+// 2.area = length * breadth
+
+#include <stdio.h>
+
+int main()
+{
+    //3.declare the variables
+    int area, length, breadth;
+    
+    //4. think about variables in memory location
+    
+    //5.assign values to variables
+    length = 5;
+    breadth = 10;
+    
+    //6.apply formula
+    area = length * breadth;
+    
+    //display output
+    printf("Area of rectangle = %d", area);
+    
+}
